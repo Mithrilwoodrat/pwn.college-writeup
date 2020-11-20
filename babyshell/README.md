8e36d6ec3b0ef0955f6c66b61ecd25b07365763d
mithrilwoodrat <mithrilwoodrat@gmail.com>
2020-11-20 17:57:12 +0800

    add some ref links

---
+++ babyshell/README.md
@@ -0,0 +1,3 @@
+https://www.exploit-db.com/papers/13224
+
+http://shell-storm.org/shellcode/files/shellcode-505.php
--- babyshell/build.sh
+++ babyshell/build.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-gcc -nostdlib -static readflag.s -o readflag.elf
-objcopy --dump-section .text=shellcode-raw readflag.elf
+if [ "$#" -eq 3 ]; then
+    echo "Using $0, input.s <shellcode-raw>"
+    exit 0
+fi
+
+source=$1
+target=$2
+
+echo "source: $source"
+echo "output: $target"
+
+# exit 0
+
+gcc -nostdlib -static $source -o tmp.elf
+objcopy --dump-section .text=$target tmp.elf
 cat shellcode-raw|disasm -c 'amd64' > shellcode.disasm
